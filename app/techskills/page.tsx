ef95767c644387bbf90be97077330b69cb670e19
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-25 10:47:35 -0500

    contact, project, skills page

---
+++ app/techskills/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TechSkills = () => {
+  return (
+    <div className="h-screen w-full">
+      <div className="mx-auto flex h-full w-full max-w-[1000px] flex-col items-center justify-center p-4">
+        <p className="inline text-4xl font-bold text-slate-400">Skills</p>
+        <p className="py-4 text-center text-gray-200">
+          As a frontend developer, I have experience with these technologies.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TechSkills;
