6b221cec9ef603df375c908099ddcb9694f58d81
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-25 13:55:23 -0500

    map over project data to create the list

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import Project from "./project";
+import projectdata from "./projectdata";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div id="projects" className="mt-20 w-full lg:h-screen">
+        <div className="mx-auto flex h-full w-full max-w-[1000px] flex-col justify-center p-4">
+          <p className="text-center text-4xl font-bold text-gray-200">
+            Projects
+          </p>
+          <div className="flex shrink flex-col items-center justify-center gap-12 p-4 lg:flex-row">
+            {projectdata.map((p) => (
+              <Project
+                id={0}
+                key={p.id}
+                title={p.title}
+                image={p.image}
+                alt={p.alt}
+                techList={p.techList}
+                description={p.description}
+                live={p.live}
+                repo={p.repo}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
