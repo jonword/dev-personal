d7750b4d7a89b3d2819840bfb7552da41520df39
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-25 15:19:58 -0500

    icon images

---
+++ app/techskills/icons.ts
@@ -0,0 +1,59 @@
+import html from "../assets/images/skills/html.png";
+import css from "../assets/images/skills/css.png";
+import tailwind from "../assets/images/skills/tailwind.png";
+import js from "../assets/images/skills/javascript.png";
+import ts from "../assets/images/skills/ts-logo-256.png";
+import react from "../assets/images/skills/react.png";
+import next from "../assets/images/skills/next.png";
+import redux from "../assets/images/skills/redux.png";
+import node from "../assets/images/skills/node.png";
+
+const Icons = [
+  {
+    id: 0,
+    image: html,
+    alt: "HTML",
+  },
+  {
+    id: 1,
+    image: css,
+    alt: "CSS",
+  },
+  {
+    id: 2,
+    image: tailwind,
+    alt: "Tailwindcss",
+  },
+  {
+    id: 3,
+    image: js,
+    alt: "JavaScript",
+  },
+  {
+    id: 4,
+    image: ts,
+    alt: "TypeScript",
+  },
+  {
+    id: 5,
+    image: react,
+    alt: "Reactjs",
+  },
+  {
+    id: 6,
+    image: next,
+    alt: "Nextjs",
+  },
+  {
+    id: 7,
+    image: redux,
+    alt: "Redux",
+  },
+  {
+    id: 8,
+    image: node,
+    alt: "Nodejs",
+  },
+];
+
+export default Icons;
