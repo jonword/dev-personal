4ba79eacaf47d0d40f977c1ef3975d8d7db6977d
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-05-25 13:53:12 -0500

    add form to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import Socials from "../socials/socials";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      className="flex h-screen w-full flex-col items-center justify-center p-4"
+      id="contact"
+    >
+      <h1 className="pb-4 text-center text-3xl text-white">Contact</h1>
+      <Socials />
+      <form
+        name="Contact"
+        method="POST"
+        className="flex  w-full max-w-[600px] flex-col"
+      >
+        {/* Netlify */}
+        <input type="hidden" name="form-name" value="Contact" />
+
+        <div className="">
+          <p className="pb-8 text-white"></p>
+        </div>
+        <input type="text" placeholder="Name" name="name" className="p-2" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          className="my-4 p-2"
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          className="p-2"
+          rows={10}
+        />
+        <button
+          type="submit"
+          className=" mx-auto mt-2 flex items-center border-2 border-gray-400 px-12 py-4 text-lg font-bold text-white hover:bg-green-700 duration-200"
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
