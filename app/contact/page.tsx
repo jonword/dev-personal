71fe6a3daf6973e44d758c4e490faca2d29f9a8c
Jonathan Word <91210536+jonword@users.noreply.github.com>
2023-06-08 08:49:43 -0500

    create contact form component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,8 +5,8 @@ import { z } from "zod";
 const Contact = () => {
   return (
     <div
-      className="flex min-h-screen w-full flex-col p-24"
-      id="contact items-center"
+      className="flex min-h-screen w-full flex-col p-24 items-center"
+      id="contact"
     >
       <h1 className="pb-4 text-3xl text-white">Contact</h1>
       <form
